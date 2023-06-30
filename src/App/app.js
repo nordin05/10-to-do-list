bd941b54d6b1a1cc6d111264087146bd00639521
Nordin <128930417+nordin05@users.noreply.github.com>
2023-06-30 04:12:57 +0200

    Dont open description when checkbox clicked

--- src/App/app.js
+++ src/App/app.js
@@ -57,13 +57,16 @@ function hideDescription(parent, child) {
 tasks.addEventListener("click", function (e) {
     const remove_task_btn = e.target.closest(".close-btn");
     const task_item = e.target.closest(".taskItem");
+    const checkbox = e.target.closest("#checkbox");
 
     if (remove_task_btn) {
         const taskName = remove_task_btn.parentNode.children[0].innerHTML;
         currentActiveProject.removeTask(taskName);
         renderActiveProjectTasks(currentActiveProject.name);
     }
-    if (task_item) {
+    if (checkbox) {
+        return;
+    } else if (task_item) {
         const description = task_item.querySelector(".description");
         const display = window.getComputedStyle(description).display;
         if (display == "none") {
